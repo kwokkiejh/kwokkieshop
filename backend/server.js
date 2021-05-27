5618c619a98d151fb2d59dde9db57362215f1a4c
kwokkiejh <kwokkiejiahui@gmail.com>
2021-05-27 12:54:56 +0800

    try new again

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("server running on port 5000, try new"));
+app.listen(PORT, console.log("server running on port 5000, try new agains"));
