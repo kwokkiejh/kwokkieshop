bf9205de4662ad256b7fd08d99e6bfaad494a477
kwokkiejh <kwokkiejiahui@gmail.com>
2021-07-24 16:19:20 +0800

    updated axios base url

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.baseURL =
-    "http://kwokkieshopbackendnodejs-env.eba-835m3mjm.us-east-2.elasticbeanstalk.com/";
+axios.defaults.baseURL = "https://kwokkieshop-backend.herokuapp.com/";
 
 ReactDOM.render(
     <Provider store={store}>
